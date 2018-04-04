91c98b24a18b93fde55da99bcc1533b1b69406d1
timaf <lo_sa_na@yahoo.com>
2018-04-04 12:17:52 +0200

    start

---
+++ Nurseryo/src/main/java/at/refugeescode/Nurseryo/NurseryoApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Nurseryo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseryoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseryoApplication.class, args);
+	}
+}
